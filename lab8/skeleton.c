c1a8c922898fe914043a1b46f536a54fe2c88841
tspython <tspython@hotmail.com>
2024-03-06 17:18:34 -0800

    lab8 init

---
+++ lab8/skeleton.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+// output FIFO:  9515
+//
+
+typedef struct {//to 
+    int pageno;
+} ref_page;
+
+
+int main(int argc, char *argv[]){
+    int CACHE_SIZE = atoi(argv[1]);
+    ref_page cache[CACHE_SIZE];
+    char pageCache[100];
+
+    int i;
+    int totalFaults = 0;
+
+    int placeInArray = 0;
+
+    for (i = 0; i < CACHE_SIZE; i++){
+         cache[i].pageno = -1;
+    }
+
+    while (fgets(pageCache, 100, stdin)){
+        int page_num = atoi(pageCache);
+        bool foundInCache = false;
+        for (i=0; i< CACHE_SIZE; i++){
+            if (cache[i].pageno == page_num){
+                foundInCache = true;
+                break;
+            }
+        }
+        if (foundInCache == false){
+            totalFaults++;
+            cache[placeInArray].pageno = page_num;
+            placeInArray = (placeInArray + 1) % CACHE_SIZE;
+        }
+
+    }
+
+    printf("%d Total Page Faults", totalFaults);
+    return 0;
+}
---
+++ lab8/test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define CACHE_SIZE 10
+
+typedef struct {
+    int pageno;
+} ref_page;
+
+int main(int argc, char *argv[]) {
+    int CACHE_SIZE = atoi(argv[1]); // Size of Cache passed by user
+    ref_page cache[CACHE_SIZE]; // Cache that stores pages
+    char pageCache[100]; // Cache that holds the input from test file
+
+    int i;
+    int totalFaults = 0; // keeps track of the total page faults
+    int front = 0; // Pointer to the first element in the cache
+    int rear = 0;  // Pointer to the next available position in the cache
+
+    for (i = 0; i < CACHE_SIZE; i++) {
+        cache[i].pageno = -1; // Initialize cache
+    }
+
+    while (fgets(pageCache, 100, stdin)) {
+        int page_num = atoi(pageCache); // Stores number read from file as an int
+
+        // Check if page is already in cache
+        int pageFound = 0;
+        for (i = 0; i < CACHE_SIZE; i++) {
+            if (cache[i].pageno == page_num) {
+                pageFound = 1;
+                break;
+            }
+        }
+
+        if (!pageFound) { // Page fault
+            totalFaults++;
+            if (rear < CACHE_SIZE) {
+                cache[rear].pageno = page_num;
+                rear++;
+            } else {
+                // Replace the oldest page (FIFO)
+                cache[front].pageno = page_num;
+                front = (front + 1) % CACHE_SIZE; // Move front pointer circularly
+            }
+        }
+    }
+
+    printf("%d Total Page Faults\n", totalFaults);
+    return 0;
+}
