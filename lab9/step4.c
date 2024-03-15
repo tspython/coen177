1595676321ee2658da5465787bc619c6476cd5a5
tspython <tspython@hotmail.com>
2024-03-15 16:19:37 -0700

    ok

--- lab9/step4.c
+++ lab9/step4.c
@@ -1,57 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <input_file_path> <buffer_size>\n", argv[0]);
-        return 1;
+    FILE *fp, *fp_out;
+    char *buffer;
+    int bufferSize;
+    char outputFileName[256];
+
+    bufferSize = atoi(argv[2]);
+
+    buffer = (char *)malloc(bufferSize);
+    
+    fp = fopen(argv[1], "rb");
+    
+
+    snprintf(outputFileName, sizeof(outputFileName), "%s_copy", argv[1]);
+    fp_out = fopen(outputFileName, "wb");
+    
+
+    while (fread(buffer, 1, bufferSize, fp) > 0) {
+        fwrite(buffer, 1, bufferSize, fp_out);
     }
 
-    FILE *input_fp, *output_fp;
-    clock_t start, end;
-    double cpu_time_used;
-
-    input_fp = fopen(argv[1], "rb");
-    if (input_fp == NULL) {
-        printf("Error opening input file.\n");
-        return 1;
-    }
-
-    int buffer_size = atoi(argv[2]);
-    char buffer[buffer_size];
-    size_t bytes_read;
-
-    start = clock();
-
-    // Loop until end of input file
-    while ((bytes_read = fread(buffer, 1, buffer_size, input_fp)) > 0) {
-        // Open a new file for writing
-        char output_filename[100];
-        sprintf(output_filename, "output_%zu.bin", ftell(input_fp)); // Unique file name based on position in input file
-        output_fp = fopen(output_filename, "wb");
-        if (output_fp == NULL) {
-            printf("Error opening output file.\n");
-            fclose(input_fp);
-            return 1;
-        }
-
-        // Write the read data to the output file
-        fwrite(buffer, 1, bytes_read, output_fp);
-
-        // Close the output file
-        fclose(output_fp);
-    }
-
-    end = clock();
-
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("Time taken for I/O with buffer size %d: %f seconds\n", buffer_size, cpu_time_used);
-
-    // Close the input file
-    fclose(input_fp);
-
+    fclose(fp);
+    fclose(fp_out);
+    free(buffer);
     return 0;
 }
-
--- lab9/step5.sh
+++ lab9/step5.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for file in file1.txt file2.txt file3.txt file4.txt file5.txt
+for file in file1.txt file2.txt file3.txt file4.txt 
 do
-    for buffer in 100 1000 10000 100000
+    for buffer in 100000
     do
-        for thread in 2 8 32 64
+        for thread in 2 64
         do
             echo "Step5 $file $buffer $thread"
             time ./Step5 $file $buffer $thread
